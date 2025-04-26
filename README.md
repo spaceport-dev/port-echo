7820799d4f2ffc888f1ef6ae3273053c811a3630
jeremy <jeremy@ssdbg.com>
2025-04-26 15:24:15 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Port-Echo
+
+Port-Echo is a Spaceport starter kit that provides the basic structure and configuration
+for building a Spaceport application, with no other assumptions.
+
+## Getting Started
+
+If this is your first time using Spaceport, perhaps consider starting with the PORT-MERCURY,
+or PORT-PIONEER starter kits, which provide the same basic structure and configuration, but
+provide some basic functionality to get you started. Use Port-Echo if you want to start
+from scratch and want to implement the basics yourself.
+
+## Features
+
+- Basic structure and configuration for a Spaceport application
+- No assumptions about the application
+- Ready to launch, just add your own code.
---
+++ modules/Echo.groovy
@@ -0,0 +1,39 @@
+import spaceport.bridge.Command
+import spaceport.computer.alerts.Alert
+import spaceport.computer.alerts.results.HttpResult
+import spaceport.computer.alerts.results.Result
+
+
+/**
+ *
+ *   _              /_
+ * _)/)(/(_(-/)()/-/   ECHO, a minimal Spaceport scaffold
+ *  /       /    v2
+ *
+ * Port-Echo is a Spaceport starter kit that provides the basic structure and configuration
+ * for building a Spaceport application, with no other assumptions.
+ *
+ * While this scaffold is the most minimal starting point, if you are a new Spaceport
+ * developer you may consider starting with the Mercury or Pioneer scaffolds which build in
+ * some basic functionality and structure, with example code to speed up development.
+ *
+ * See also: https://www.spaceport.com.co/docs/scaffolds#echo
+ *
+ */
+class Echo {
+
+
+    @Alert('on initialized')
+    static _init(Result r) {
+        Command.debug('Echo initialized.')
+    }
+
+
+    // A simple HTTP route to the root
+    @Alert('on / hit')
+    static _root(HttpResult r) {
+        r.setStatus(503) // Service (temporarily) unavailable.
+    }
+
+
+}
